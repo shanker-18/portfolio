1a324c3ddfb579a1bf6159b0482637cc428b4594
Manian VJS <manian.vjs@gmail.com>
2025-06-21 17:37:50 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 5ce3cbf8c2f08b45c25b48b676b435d7b1c02ead
 } 
